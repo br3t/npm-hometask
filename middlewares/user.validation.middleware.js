b92717808141fc90c2ca33ca03c6f25e767ade0b
br3t <bret.snk@gmail.com>
2022-06-14 22:54:01 +0300

    Add user uniqueness validation at user create

--- middlewares/user.validation.middleware.js
+++ middlewares/user.validation.middleware.js
@@ -1,4 +1,6 @@
 const { user } = require('../models/user');
+const UserService = require('../services/userService');
+
 const createUserValid = (req, res, next) => {
     // TODO: Implement validatior for user entity during creation
     req.errorMessage = '';
@@ -16,7 +18,17 @@ const createUserValid = (req, res, next) => {
                 req.errorMessage += `${ruleData.errorMessage};\n`;
             }
         });
-        // TODO: check user uniqueness
+        // basic validation is OK, let's check user uniqueness
+        if(req.errorMessage == '') {
+            const sameEmailUser = UserService.search({email:req.params.email});
+            if(sameEmailUser) {
+                req.errorMessage += `User with email "${req.params.email}" is already exist;\n`;
+            }
+            const samePhoneUser = UserService.search({phoneNumber:req.params.phoneNumber});
+            if(samePhoneUser) {
+                req.errorMessage += `User with phoneNumber "${req.params.phoneNumber}" is already exist;\n`;
+            } 
+        }
     }
     next();
 }
